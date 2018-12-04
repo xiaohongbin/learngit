e3df2ebcae8a73f49cb5026c96e363808be85547
xiaohongbin <xiaohongbin@youyuan.com>
2018-12-04 14:17:20 +0800

    add file

---
+++ Welcome.java
@@ -0,0 +1,21 @@
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("大家好，我是肖红宾");
+		int abc=3;
+		int _abc;
+		int $;
+		//int 1abc;//错误: 不是语句
+		int 中文;
+		} 
+
+}
+
+
+/*
+学习笔记
+1.标识符
+定义，变量、类和方法
+2.字符集，编码：
+java采用unicode 字符集，2个字节表示一个字符，一个字节(byte)表示8位(bit)，一个英文字符占两个字节
+utf-8，中文3个字节表示一个字符，英文1个字节表示一个字符
+*/
